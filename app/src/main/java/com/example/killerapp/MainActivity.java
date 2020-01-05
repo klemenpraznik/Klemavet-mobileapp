d4a465a50ff3fc401a2608071ce1978519950458
klemenpraznik <52839991+klemenpraznik@users.noreply.github.com>
2020-01-05 19:59:32 +0100

    clients display

--- app/src/main/java/com/example/killerapp/MainActivity.java
+++ app/src/main/java/com/example/killerapp/MainActivity.java
@@ -7,7 +7,9 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -15,6 +17,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,87 +29,64 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private TextView mTextViewResult;
-    private RequestQueue mQueue;
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+    private TextView osebe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        mTextViewResult = findViewById(R.id.text_view_result);
-        Button buttonParse = findViewById(R.id.button_parse);
-
-        mQueue = Volley.newRequestQueue(this);
-        buttonParse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                jsonParse();
-            }
-        });
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        osebe = (TextView) findViewById(R.id.osebe);
     }
 
-    private void jsonParse(){
-        String url = "https://vodenje-racunov20191204085122.azurewebsites.net/api/clients";
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try{
-                    JSONArray jsonArray = response.getJSONArray("");
-
-                    for (int i = 0; i <jsonArray.length(); i++) {
-                        JSONObject client = jsonArray.getJSONObject(i);
-                        String fname = client.getString("name");
-                        String mail = client.getString("email");
-
-                        mTextViewResult.append(fname + ", " + mail);
-                    }
-                } catch (JSONException e){
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    String name = object.getString("name");
+                    String email = object.getString("email");
+//                    String priimek = "xxx";
+//                    String id = object.getString("id");
+//                    String mesto = object.getString("address");
+//                    String naslov = object.getString("email");
+                    data.add(name + ", " + email);
+                } catch (JSONException e) {
                     e.printStackTrace();
+                    return;
                 }
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+            osebe.setText("");
+            osebe.setMovementMethod(new ScrollingMovementMethod());
+            for (String row: data) {
+                String currentText = osebe.getText().toString();
+                osebe.setText(currentText + "\n\n" + row);
             }
-        });
-        mQueue.add(request);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+    };
 
-        return super.onOptionsItemSelected(item);
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
+
+    public void prikaziOsebe(View view) {
+        if (view != null) {
+            String url = "https://vodenje-racunov20191204085122.azurewebsites.net/api/clients";
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
     }
+
 }
